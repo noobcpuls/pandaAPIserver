ae564b0acfa606d15b57b1499558dabd418585ff
noobcpuls <gimhyeongyeom@gimhyeongyeoms-MacBook-Air.local>
2022-03-25 13:14:47 +0900

    next

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgres://uonsteyxfduahf:75f7611acecc70b4298612f838c729904f8da55e5ffea539f164495803282e60@ec2-18-210-159-154.compute-1.amazonaws.com:5432/d6mosa6s8u3112"
+SQLALCHEMY_DATABASE_URL = "postgresql://uonsteyxfduahf:75f7611acecc70b4298612f838c729904f8da55e5ffea539f164495803282e60@ec2-18-210-159-154.compute-1.amazonaws.com:5432/d6mosa6s8u3112"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
